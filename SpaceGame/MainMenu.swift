ac7fdeaeeaa44c15ecf60e0503d8aab94fc905d2
miloserdova <rac-99@mail.ru>
2020-10-31 22:09:43 +0200

    commmit 1

---
+++ SpaceGame/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  SpaceGame
+//
+//  Created by Yulia Miloserdova on 6/3/20.
+//  Copyright © 2020 Yulia Miloserdova. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: SKScene {
+
+}
